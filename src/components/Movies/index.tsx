b2712127ad0f4f18370a0045488b2db445de7e16
Alex Chika <mcluckey1@gmail.com>
2023-05-25 12:47:08 +0100

    testing collaborations settings and reviewing stats

--- src/components/Movies/index.tsx
+++ src/components/Movies/index.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 const index = () => {
   return (
-    <div>Hello netflix... setting up collaboration accounts and testing </div>
+    <div>
+      Hello netflix... setting up collaboration accounts and testing and
+      reviewing git stats{" "}
+    </div>
   );
 };
 
